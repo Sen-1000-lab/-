d58e4e5a76799137fc91c4d8572e47fb3e6ff5d9
Sen-1000-lab <t31075049@nara-su.ac.jp>
2026-05-05 20:26:21 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -163,7 +163,7 @@ async def on_message(message):
          if message.content == "おみくじ":
                 res = random.choice(["大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"])
                 await message.reply(f"⛩️ おみくじの結果：**{res}** です！")
-            elif message.content == "ランくじ！":
+        elif message.content == "ランくじ！":
                 # 167行目の末尾に  を付けるのを忘れずに！
                 res = random.choices(["💎 ランク当たり", "✨ 大当たり", "✴️ 中当たり", "✳️ 小当たり", "💀 ハズレ"], weights=[1, 2, 5, 10, 82], k=1)[0]
                 await message.reply(f"🎲 抽選結果：**{res}**")
